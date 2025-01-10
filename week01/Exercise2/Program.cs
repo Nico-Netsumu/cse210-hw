b55970f6e8c3de48e8dfe1bdddf02ef5f7d23d28
Nico Netsumu <nicobobytph@gmail.com>
2025-01-10 12:51:25 -0500

    changed1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int x = 7;
+        int y = 3;
+        
+        if (x > y)
+            {
+                Console.WriteLine($"{x} is more than {y}");
+            }
+        else
+            {
+                Console.WriteLine($"{x} is less than {y}");
+            }
     }
 }
