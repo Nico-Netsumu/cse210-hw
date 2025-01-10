aabe6c7d9def1bd2081a9e3c94dd981eef5baa8c
Nico Netsumu <nicobobytph@gmail.com>
2025-01-10 13:04:44 -0500

    changed1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,16 +5,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 7;
-        int y = 3;
-        
-        if (x > y)
-            {
-                Console.WriteLine($"{x} is more than {y}");
-            }
+        Console.WriteLine("Welcome to the class grade dataminer!");
+        Console.Write("What is your percentage on this class? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
         else
-            {
-                Console.WriteLine($"{x} is less than {y}");
-            }
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"The grade letter you got is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You Successfully passed to the next class!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! ");
+        }
+
     }
 }
