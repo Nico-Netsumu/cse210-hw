9055af014b6eb1e34d225a9a86fb783d00b3daab
Nico Netsumu <nicobobytph@gmail.com>
2025-01-10 18:38:10 -0500

    changed1

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string usern = promptUserName();
+        int userNumb = promptUserNumber();
+
+        int squaredNumber = squareNumber(userNumb);
+
+        DisplayResult(usern, squaredNumber);
     }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to my program!");
+    }
+
+    static string promptUserName()
+    {
+        Console.Write("Please enter your full name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int promptUserNumber()
+    {
+        Console.Write("Enter your favorite number! ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"So you are '{name}', did you know that");
+        Console.WriteLine($"your favoritenumber times itself is {square}?");
+    }
+
 }
