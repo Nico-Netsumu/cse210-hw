37794a75094fdb4f15c479b051f0adb1342c318c
Nico Netsumu <nicobobytph@gmail.com>
2025-01-10 12:16:49 -0500

    changed1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome to my fiirst program in C#!");
+        Console.Write("What is your first name? ");
+        string firstn = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lastn = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {firstn}, {firstn} {lastn}.");
     }
 }
