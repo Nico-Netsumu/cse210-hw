9f28ad7258a22d7fd02bddf3945a356d4f5cc896
Nico Netsumu <nicobobytph@gmail.com>
2025-01-20 20:30:59 -0500

    changed

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -8,7 +8,7 @@ class Program
         List<string> prompts = new List<string>
         {
             "Who was the most interesting person I interacted with today?",
-            "What was the best part of my day?",
+            "What was the best part of my day? And Why?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
             "If I had one thing I could do over today, what would it be?"
