208030f5ef48aa4eda35821b778482d2e9a309d7
Nico Netsumu <nicobobytph@gmail.com>
2025-02-10 14:06:55 -0500

    Updated1

---
+++ week06/Shapes/Functions.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+// Shapes
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
+
+// Circles
+public class Circle : Shape
+
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
+
+// Rectangle
+
+public class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectangle(string color, double length, double width) : base (color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
+
+// Square
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
